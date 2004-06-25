48a211340b028a1f1f551f978e07974b07bae8bb
Ville Bergholm <smite-meister@users.sf.net>
2004-06-25 18:02:47 +0000

    Cleanup
    git-svn-id: https://svn.code.sf.net/p/doomlegacy/svn/legacy/trunk@113 5da24444-3c17-0410-b9f4-a1bbb4243164

---
+++ engine/g_team.cpp
@@ -0,0 +1,40 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 2003-2004 by DooM Legacy Team.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+//
+// $Log$
+// Revision 1.1  2004/06/25 18:02:47  smite-meister
+// Cleanup
+//
+//
+//-----------------------------------------------------------------------------
+
+/// \file
+/// \brief Team system
+
+#include "g_team.h"
+
+
+/// constructor
+TeamInfo::TeamInfo()
+{
+  name = "Romero's Roughnecks";
+  color = 0;
+  score = 0;
+}
+
+
