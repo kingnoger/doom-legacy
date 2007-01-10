968fba7c7ed6419d66393416e19bbdc36d55b8e4
Jussi Pakkanen <jussip@users.sf.net>
2007-01-10 23:06:32 +0000

    Allow SDL to autodetect 16 bpp OpenGL modes.
    git-svn-id: https://svn.code.sf.net/p/doomlegacy/svn/legacy/trunk@418 5da24444-3c17-0410-b9f4-a1bbb4243164

--- video/hardware/oglrenderer.cpp
+++ video/hardware/oglrenderer.cpp
@@ -239,10 +239,6 @@ bool OGLRenderer::InitVideoMode(const int w, const int h, const bool fullscreen)
   if(fullscreen)
     surfaceflags |= SDL_FULLSCREEN;
 
-  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-  //  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
