408c9487a9741f6f05a651e27bdf0ea5a9703ebe
Ville Bergholm <smite-meister@users.sf.net>
2008-01-20 23:20:02 +0000

    Raven licence fixed.
    git-svn-id: https://svn.code.sf.net/p/doomlegacy/svn/legacy/trunk@523 5da24444-3c17-0410-b9f4-a1bbb4243164

--- engine/p_henemy.cpp
+++ engine/p_henemy.cpp
@@ -6,15 +6,13 @@
 // Copyright (C) 1993-1996 by Raven Software, Corp.
 // Copyright (C) 1998-2007 by DooM Legacy Team.
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
+// All the source code within this file authored by the Doom Legacy Team
+// falls under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// The rest of the source code in this file is subject to the terms
+// of the Activision EULA (docs/activision_eula.txt), where applicable.
 //
 //-----------------------------------------------------------------------------
 
